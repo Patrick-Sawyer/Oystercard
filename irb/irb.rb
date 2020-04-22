238d81373825ab6fe05e08fb858ebdbf6142174a
Catriona <catrionamairi@blueyonder.co.uk>
2020-04-22 15:55:53 +0100

    Added to irb feature test

--- irb/irb.rb
+++ irb/irb.rb
@@ -2,5 +2,10 @@ require './lib/oystercard'
 
 card = Oystercard.new
 card.top_up(6)
-card.touch_in
+puts card.in_journey?
+card.touch_in("Holborn")
+puts card.in_journey?
+puts card.entry_station
 card.touch_out
+puts card.entry_station
+puts card.in_journey?
